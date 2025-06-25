ccb747ce33d29c20d68d16ca280de040d284b694
makwanaavi <makwanaavi73@gmail.com>
2025-06-25 10:57:17 +0530

    Add Home Screen

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
+    return (
+        <div>
+            About page
+        </div>
+    )
 }
 
 export default page
