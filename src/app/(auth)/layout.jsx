c348288dea6669676e410908fb547b7f66ff6bce
makwanaavi <makwanaavi73@gmail.com>
2025-06-25 12:19:17 +0530

    #update

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,28 @@
+"use client"
+import React from "react"
+import Logo from '../../components/Logo'
+import Textanimation from "../../components/Textanimation"
+
+
+const Authlayout = ({ children }) => {
+    return (
+        <div className="grid lg:grid-cols-2 min-h-screen max-h-screen h-full">
+            <div className="hidden lg:flex flex-col p-10 bg-primary/10">
+                <div className="flex items-center ">
+                    <Logo />
+                </div>
+                <div className="h-full flex flex-col justify-center">
+                    <Textanimation
+                        className="flex-row mx-0 lg:gap-2"
+                    />
+                </div>
+            </div>
+
+            <div className="h-full flex flex-col mt-14 lg:mt-0 lg:justify-center px-4 lg:p-6 overflow-auto">
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Authlayout
--- src/components/Textanimation.jsx
+++ src/components/Textanimation.jsx
@@ -1,37 +1,44 @@
-"use client"
-import { TypeAnimation } from 'react-type-animation';
+"use client";
+import { TypeAnimation } from "react-type-animation";
 
 const Textanimation = () => {
     return (
-        <TypeAnimation
-            sequence={[
-                "Hey, ready to bring your ideas to life?",
-                1000,
-                "Hey, let's design something amazing!",
-                1000,
-                "Hey, welcome to the world of web magic!",
-                1000,
-                "Hey, code smarter, not harder!",
-                1000,
-                "Hey, your journey to React mastery starts here!",
-                1000,
-                "Hey, unleash your creativity with HTML & CSS!",
-                1000,
-                "Hey, turn your imagination into reality!",
-                1000,
-                "Hey, let's build beautiful interfaces together!",
-                1000,
-                "Hey, time to innovate with JavaScript!",
-                1000,
-                "Hey, design. Develop. Dominate.",
-                1000,
-            ]}
-            wrapper="span"
-            speed={50}
-            style={{ fontSize: '1.5em', display: 'inline-block' }}
-            repeat={Infinity}
-        />
+        <div className="mx-auto text-2xl lg:text-5xl my-6 flex-col gap-3 lg:gap-5 font-bold text-center">
+            <div className="tex">Build Your Cureer</div>
+            <div className="w-fit mx-auto text-center">
+                <TypeAnimation
+                    sequence={[
+                        "Hey, ready to bring your ideas to life?",
+                        1000,
+                        "Hey, let's design something amazing!",
+                        1000,
+                        "Hey, welcome to the world of web magic!",
+                        1000,
+                        "Hey, code smarter, not harder!",
+                        1000,
+                        "Hey, your journey to React mastery starts here!",
+                        1000,
+                        "Hey, unleash your creativity with HTML & CSS!",
+                        1000,
+                        "Hey, turn your imagination into reality!",
+                        1000,
+                        "Hey, let's build beautiful interfaces together!",
+                        1000,
+                        "Hey, time to innovate with JavaScript!",
+                        1000,
+                        "Hey, design. Develop. Dominate.",
+                        1000,
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{ fontSize: "1em", display: "inline-block" }}
+                    repeat={Infinity}
+                    className="text-primary"
+                />
+            </div>
+        </div>
+
     );
 };
 
-export default Textanimation; 
+export default Textanimation;
