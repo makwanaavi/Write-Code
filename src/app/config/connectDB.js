b0e3b3a96eec27675d749e5aa2f0bd6fa21c2265
makwanaavi <makwanaavi73@gmail.com>
2025-06-25 15:48:19 +0530

    #update

---
+++ src/app/config/connectDB.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+// import { buffer } from "stream/consumers";
+
+const MONGODB_URL = process.env.MONGODB_URL
+
+if (!MONGODB_URL) {
+    throw new Error("Please define mongoDB Url in env file")
+}
+
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+export async function ConnectDB() {
+    if (cached.conn) {
+        return cached.conn
+    }
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+            maxPoolsize: 10
+        }
+        cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw error
+    }
+}
